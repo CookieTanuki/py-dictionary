6e3c1d3b1789a7528b8ab10dfb084419af73f6d3
Nick <nikitosi3012@gmail.com>
2025-11-18 05:00:37 +0200

    Implemented base carcass

--- app/main.py
+++ app/main.py
@@ -1,2 +1,24 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.hash = hash(key)
+
+
 class Dictionary:
-    pass
+    def __init__(self):
+        self.capacity = 0
+        self.size = 0
+
+    def __setitem__(self, key, value):
+        ...
+
+    def __getitem__(self, key):
+        ...
+
+    def __len__(self):
+        return self.size
+
+    def resize(self):
+        ...
+
